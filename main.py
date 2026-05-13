c0df3b55d463c59115157db579f3386d72de2453
MatthewF4b <matheusfabb@outlook.com>
2026-05-13 13:01:15 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from services.cupom_service import aplicar_cupom
-
-
-codigo = input("Digite o código do cupom: ")
-valor = float(input("Digite o valor da compra: "))
-
-desconto = aplicar_cupom(codigo, valor)
-
-print(f"Desconto aplicado: {desconto * 100}%")
